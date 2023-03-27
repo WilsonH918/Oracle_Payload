b9df9fde4b74c58432e24239d0d4cb401211c3ca
Wilson <117455557+WilsonH918@users.noreply.github.com>
2023-03-27 11:02:53 +0100

    Create AgenttoAgentId.sql

---
+++ SQL/AgenttoAgentId.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT 
+    pu.username AS USERNAME,
+    TO_CHAR(pu.person_id) AS AGENT_ID,
+    ppf.display_name AS DISPLAY_NAME
+FROM 
+    per_users pu,
+    per_person_names_f_v ppf
+WHERE 
+    ppf.person_id = pu.person_id
+ORDER BY 
+    pu.username
